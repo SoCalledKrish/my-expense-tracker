0ebc0fbc8bc6e4acf8e760588a881f0b3e9d76f4
SoCalledKrish <abhiram.u.2003@gmail.com>
2025-04-20 23:55:36 +0530

    added cores in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://my-expense-tracker-w1e0.onrender.com",
   credentials: true,
 }));
 app.use(express.json());
